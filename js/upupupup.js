afa68b92f65aa61a1a208c3985025ba7f2e04e6c
ldliquid <louise.drulhe@gmail.com>
2014-09-23 15:05:35 +0200

    fix bug

--- js/upupupup.js
+++ js/upupupup.js
@@ -1,24 +1,27 @@
 $('.upupupup').click(function(){
-	 $(this).html(function(index, html) {
-		 var contenu = $(this).html();
-		 var newDiv = "<div class='fly'></div>";
-		 $('body').prepend(newDiv);
-		 $(".fly").text(contenu);
-				 
-		  var nbr = contenu.length,
-				 space = "&nbsp;",
-		 		 blank = Array(nbr+1).join(space);
-	 		return html.replace(contenu,blank);
-	 });
-	var offset = $( this ).offset();
-	var coordx = offset.left;
-	var coordy = offset.top;
-	
-	var style = { "display": "block", "left": coordx + "px", "top": coordy + "px" };
-	$(".fly").css(style);
-	
+	 if ($(this).hasClass('upupupup')){
+		 $(this).html(function(index, html) {
+			 var contenu = $(this).html();
+			 var newDiv = "<div class='fly'></div>";
+			 $('body').prepend(newDiv);
+			 $('.fly').text(contenu);
+
+				var nbr = contenu.length,
+					 space = "&nbsp;",
+					 blank = Array(nbr+1).join(space);
+				return html.replace(contenu,blank);
+		 });
+		var offset = $( this ).offset();
+		var coordx = offset.left;
+		var coordy = offset.top;
+		var style = { "display": "block", "left": coordx + "px", "top": coordy + "px" };
+		$(".fly").css(style);
+
 		setTimeout(function() {
-		$(".fly").addClass('high');
-			console.log("foo")
+			$(".fly").addClass('high');
 		}, 100);
+		$(this).removeClass('upupupup');
+	}else{
+		$(this).removeClass('upupupup');
+	} 
 })
